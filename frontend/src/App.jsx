f4249d06ab8c0c3183539bb8750f65343823eadc
Adriel Cezar <adrielcezar022@gmail.com>
2026-08-14 23:33:56 -0300

    feat: implementa formulário e POST para criação de tarefas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,36 @@ fetch("http://localhost:8080/tasks")
 .then(dados => setTarefas(dados));
 }, []);
 
+async function criarTarefa() {
+  const resposta = await fetch("http://localhost:8080/tasks", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ titulo: titulo, descricao: descricao })
+  });
+  const tarefaCriada = await resposta.json();
+  setTarefas([...tarefas, tarefaCriada]);
+  setTitulo("");
+  setDescricao("");
+}
+
   return (
     <div>
       <h1>Mini Kanban</h1>
+
+<input
+  value={titulo}
+  onChange={(e) => setTitulo(e.target.value)}
+  placeholder="Título da tarefa"
+/>
+
+<input
+  value={descricao}
+  onChange={(e) => setDescricao(e.target.value)}
+  placeholder="Descrição da tarefa"
+/>
+
+<button onClick = {criarTarefa}>AdicionarTarefa</button>
+
       <div className="coluna"><h2>A Fazer</h2>{tarefas.filter(tarefa => tarefa.status === "a_fazer").map(tarefa => (<div key = {tarefa.id}>{tarefa.titulo}</div>))}</div>
       <div className="coluna"><h2>Em Progresso</h2>{tarefas.filter(tarefa => tarefa.status === "em_progresso").map(tarefa => (<div key = {tarefa.id}>{tarefa.titulo}</div>))}</div>
       <div className="coluna"><h2>Concluido</h2>{tarefas.filter(tarefa => tarefa.status === "concluido").map(tarefa => (<div key = {tarefa.id}>{tarefa.titulo}</div>))}</div>
